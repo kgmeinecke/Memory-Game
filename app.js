6e02676d1dfdfd6c493e8e0022f4953dc2065b31
Kelton Meinecke <kmeinecke@gmail.com>
2020-03-17 13:29:09 -0600

    game is working

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ let numCards = gameCards.length,
   currentScore = 0,
   cardsFlipped = 0,
   card1 = null,
-  card2 = null;
+  card2 = null,
+  lowScore = localStorage.getItem("low-score");
+
+if (lowScore) {
+  document.getElementById("best-score").innerText = lowScore;
+}
 
 // ===========================
 //  Add listener to each Card
@@ -22,6 +27,42 @@ startBtn.addEventListener("click", startGame);
 // ======================
 //      functions
 // ======================
+
+function clickCard(e) {
+  if (!e.target.classList.contains("front")) return;
+
+  let currentCard = e.target.parentElement;
+
+  if (!card1 || !card2) {
+    if (!currentCard.classList.contains("flipped")) {
+      setScore(currentScore + 1);
+    }
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  if (card1 && card2) {
+    let gif1 = card1.children[1].children[0].src;
+    let gif2 = card2.children[1].children[0].src;
+
+    if (gif1 === gif2) {
+      cardsFlipped += 2;
+      card1.removeEventListener("click", clickCard);
+      card2.removeEventListener("click", clickCard);
+      card1 = null;
+      card2 = null;
+    } else {
+      setTimeout(function() {
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        card1 = null;
+        card2 = null;
+      }, 1000);
+    }
+  }
+  if (cardsFlipped === numCards) endGame();
+}
 
 function startGame() {
   start.classList.add("playing");
@@ -51,10 +92,21 @@ function shuffle(array) {
   return newArray;
 }
 
-function clickCard(e) {
-  if (!e.target.classList.contains("front")) return;
+// Set Score
+function setScore(newScore) {
+  currentScore = newScore;
+  document.querySelector("#current-score").innerText = currentScore;
+}
 
-  let currentCard = e.target.parentElement;
-
-  currentCard.classList.add("flipped");
+// End Game
+function endGame() {
+  let end = document.querySelector("#end");
+  let scoreHeader = end.children[1];
+  scoreHeader.innerText = "Your score: " + currentScore;
+  let lowScore = +localStorage.getItem("low-score") || Infinity;
+  if (currentScore < lowScore) {
+    scoreHeader.innerText += " - NEW BEST SCORE!!";
+    localStorage.setItem("low-score", currentScore);
+  }
+  document.getElementById("end").classList.add("game-over");
 }
